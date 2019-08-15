e90281d9cdce984897ce76f96bb9c9813a8e5bf0
linyows <linyows@gmail.com>
2019-08-15 15:52:20 +0900

    init commit

---
+++ nuxt.config.ts
@@ -0,0 +1,39 @@
+import NuxtConfiguration from '@nuxt/config'
+
+const config: NuxtConfiguration = {
+  mode: 'spa',
+  head: {
+    title: process.env.npm_package_name || '',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  loading: { color: '#fff' },
+  css: [
+  ],
+  plugins: [
+  ],
+  devModules: [
+  ],
+  modules: [
+    '@nuxtjs/bulma',
+  ],
+  build: {
+    postcss: {
+      preset: {
+        features: {
+          customProperties: false
+        }
+      }
+    },
+    extend (config, ctx) {
+    }
+  }
+}
+
+export default config

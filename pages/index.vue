51abd89fc4794e6491d5e8089f049ae9165bf06e
linyows <linyows@gmail.com>
2019-08-16 23:03:36 +0900

    :police_car:

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,9 @@
       <h1><logo /></h1>
       <!-- <h2 class="subtitle">福岡市西区にある行政書士事務所です</h2> -->
       <p>〒819-0001 福岡市西区小戸5-10-20<br> TEL/FAX:  <a href="tel:0928348834">092-834-8834</a> </p>
-      <p class="contact"><a class="button is-medium" href="https://docs.google.com/forms/d/e/1FAIpQLSfQNI2nAEvKfKxmx8We5KT10foX48iAH18bsh0182Ew4N-J3g/viewform">お問い合わせ</a></p>
+      <p class="contact">
+        <a class="button is-medium" href="https://docs.google.com/forms/d/e/1FAIpQLSfQNI2nAEvKfKxmx8We5KT10foX48iAH18bsh0182Ew4N-J3g/viewform">お問い合わせ</a>
+      </p>
     </div>
   </div>
 </template>

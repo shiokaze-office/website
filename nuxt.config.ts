6867f412f317605597d7ac80655bd58122a3babe
linyows <linyows@gmail.com>
2019-08-18 18:38:54 +0900

    update viewport

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,7 +7,7 @@ const config: NuxtConfiguration = {
     titleTemplate: `%s - ${process.env.npm_package_fullname}`,
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no' },
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [

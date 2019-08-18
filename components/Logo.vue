478521852c97f91ce99fe56693532210aca37ceb
linyows <linyows@gmail.com>
2019-08-18 23:02:40 +0900

    add props for logo

--- components/Logo.vue
+++ components/Logo.vue
@@ -1,14 +1,39 @@
 <template>
-  <div>
-    <h1 class="logo"><img src="~/assets/shiokaze-logo-vertical.svg" alt="行政書士 しおかぜ事務所"> </h1>
-  </div>
+  <component v-bind:is="computedTag" class="logo"> <img :src="computedImage" :alt="imgAlt"> </component>
 </template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'nuxt-property-decorator'
+
+@Component
+export default class Logo extends Vue {
+  public imgAlt: string = '行政書士 しおかぜ事務所'
+
+  @Prop() public vh!: string
+  @Prop() public tag!: string
+
+  public get computedImage() {
+    if (this.vh === undefined) {
+      this.vh = 'horizontal'
+    }
+    return require(`~/assets/img/shiokaze-logo-${this.vh}.svg`)
+  }
+
+  public get computedTag() {
+    if (this.tag === undefined) {
+      this.tag = 'span'
+    }
+    return `${this.tag}`
+  }
+}
+</script>
 
 <style scoped>
 .logo {
-  width: 100%;
+  width: 400px;
   max-height: 290px;
   margin: 0 auto;
+  display: block;
 }
 @media (max-width: 600px) {
   .logo {

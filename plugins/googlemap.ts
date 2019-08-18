bd169b1d7fefdff0fb980801454fc2ba7f74d12a
linyows <linyows@gmail.com>
2019-08-18 18:35:56 +0900

    change google map key

--- plugins/googlemap.ts
+++ plugins/googlemap.ts
@@ -3,7 +3,7 @@ import * as VueGoogleMaps from 'vue2-google-maps'
 
 Vue.use(VueGoogleMaps, {
   load: {
-    key: 'AIzaSyD7BFJKUOBD_2jOXVS62p5MQ2nOon2-5qo',
+    key: 'AIzaSyCxWeezNfUlaG2bDgvDpwpf3K_hsbzk5oA',
     libraries: 'places'
   }
 })

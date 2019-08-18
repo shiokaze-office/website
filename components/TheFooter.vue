26e326eedb86e19e091d94764f27ada3a4ca65c4
linyows <linyows@gmail.com>
2019-08-18 18:26:53 +0900

    add footer component

---
+++ components/TheFooter.vue
@@ -0,0 +1,21 @@
+<template>
+  <footer class="thefooter">
+    <p>&copy; 2019 shiokaze.net</p>
+  </footer>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+@Component
+export default class TheFooter extends Vue {
+}
+</script>
+
+<style scoped>
+.thefooter {
+  text-align: center;
+  font-weight: bold;
+  font-size: .85em;
+  margin: 2em 0;
+}
+</style>

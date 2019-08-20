985e3da1920b185090bac1c48156e164737733ca
linyows <linyows@gmail.com>
2019-08-20 23:05:16 +0900

    target blank

--- components/About.vue
+++ components/About.vue
@@ -7,7 +7,7 @@
       〒819-0001 福岡市西区小戸5-10-20<br> TEL/FAX:  <a href="tel:0928348834">092-834-8834</a>
     </p>
     <p class="contact">
-      <a class="button is-medium" href="https://docs.google.com/forms/d/e/1FAIpQLSfQNI2nAEvKfKxmx8We5KT10foX48iAH18bsh0182Ew4N-J3g/viewform">お問い合わせ</a>
+      <a class="button is-medium" target="_blank" href="https://docs.google.com/forms/d/e/1FAIpQLSfQNI2nAEvKfKxmx8We5KT10foX48iAH18bsh0182Ew4N-J3g/viewform">お問い合わせ</a>
     </p>
   </section>
 </template>

242c7d680bab5ade26b5e47c55006b73e6e0385b
linyows <linyows@gmail.com>
2020-02-08 15:24:52 +0900

    contain logo in footer

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,13 +1,30 @@
 <template>
   <footer class="thefooter">
-    <p>&copy; 2014-2020 行政書士 しおかぜ事務所</p>
+    <Logo />
+    <p><Fa :icon="['far', 'copyright']" /> 2014-{{ currentYear }} 行政書士 しおかぜ事務所</p>
   </footer>
 </template>
+
+<script lang="ts">
+import { createComponent } from '@vue/composition-api'
+import Logo from '~/components/Logo.vue'
+
+export default createComponent({
+  components: {
+    Logo
+  },
+  setup () {
+    const currentYear = new Date().getFullYear()
+    return { currentYear }
+  }
+})
+
+</script>
 
 <style scoped>
 .thefooter {
   text-align: center;
   font-size: .7em;
-  margin: 2em 0;
+  margin: 2em 0 1em;
 }
 </style>

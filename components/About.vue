ae945e94bfd1a8c8e7ed33ee33a2e472e8526c7b
linyows <linyows@gmail.com>
2020-02-08 15:23:26 +0900

    split to office page

--- components/About.vue
+++ components/About.vue
@@ -3,27 +3,11 @@
     <p class="body">
       しおかぜ事務所は、成年後見、遺言、生前・死後事務委任、信託を利用して、人生の終わりをサポートする行政書士事務所です。
     </p>
-    <p class="address">
-      〒819-0001 福岡市西区小戸5-10-20<br> TEL/FAX:  <a href="tel:0928348834">092-834-8834</a>
-    </p>
-    <p class="contact">
-      <a class="button is-medium" target="_blank" href="https://docs.google.com/forms/d/e/1FAIpQLSfQNI2nAEvKfKxmx8We5KT10foX48iAH18bsh0182Ew4N-J3g/viewform">お問い合わせ</a>
-    </p>
   </section>
 </template>
 
 <style scoped>
 .body {
   margin: 1em 0 1em;
-}
-
-.address {
-  font-size: 1em;
-  line-height: 2;
-  margin: 0 0 2em;
-}
-
-.contact {
-  margin: 0 0 3em;
 }
 </style>
---
+++ pages/office.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div class="container">
+      <Office />
+    </div>
+    <Map />
+  </div>
+</template>
+
+<script lang="ts">
+import { createComponent } from '@vue/composition-api'
+import Office from '~/components/Office.vue'
+import Map from '~/components/Map.vue'
+
+export default createComponent({
+  components: {
+    Office,
+    Map
+  },
+  setup () {
+    return { }
+  }
+})
+</script>
+
+<style>
+</style>

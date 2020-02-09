4331dfd11ae91b155fa1c8aa76055a897bac2610
linyows <linyows@gmail.com>
2020-02-09 16:39:10 +0900

    add blog component

---
+++ pages/blog/_slug.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <Blog :name="slug" />
+  </div>
+</template>
+
+<script lang="ts">
+import { createComponent, SetupContext } from '@vue/composition-api'
+import Blog from '~/components/Blog.vue'
+export default createComponent({
+  components: {
+    Blog
+  },
+  setup (_, context: SetupContext) {
+    const slug = context.root.$root.$route.params.slug
+    return { slug }
+  }
+})
+</script>
+
+<style module>
+</style>

1696ea198bfdbcb15ae05faeb75710fe4c7eb31e
linyows <linyows@gmail.com>
2020-02-09 23:17:13 +0900

    add media compoent

---
+++ components/TheMedia.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="the-media">
+    <the-img :path="name" :alt="alt" :caption="caption" />
+  </div>
+</template>
+
+<script lang="ts">
+import { createComponent } from '@vue/composition-api'
+import TheImg from '~/components/TheImg.vue'
+
+export default createComponent({
+  components: {
+    TheImg
+  },
+  props: {
+    name: {
+      type: String,
+      required: true
+    },
+    alt: {
+      type: String
+    },
+    caption: {
+      type: String
+    }
+  },
+  setup () {
+    return { }
+  }
+})
+</script>
+
+<style lang="postcss" scoped>
+</style>

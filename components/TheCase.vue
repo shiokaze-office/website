12afdb4d69f53c7a0e5070e643484cc077f86269
linyows <linyows@gmail.com>
2020-02-15 23:11:19 +0900

    add case component

---
+++ components/TheCase.vue
@@ -0,0 +1,28 @@
+<template>
+  <p class="the-case">
+    {{ name }}
+  </p>
+</template>
+
+<script lang="ts">
+import { createComponent } from '@vue/composition-api'
+
+export default createComponent({
+  props: {
+    name: {
+      type: String,
+      default: 'Case☝️'
+    }
+  },
+  setup () {
+    return {}
+  }
+})
+</script>
+
+<style scoped>
+  .the-case {
+    font-weight: bold;
+    padding-bottom: .2em;
+  }
+</style>

a201fe025457da88f9f05b5794847a47608be589
linyows <linyows@gmail.com>
2020-02-17 22:10:14 +0900

    integrate blog for index and slug

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container">
-    <article>
+    <article v-if="slug">
       <header>
         <h1>{{ attr.title }}</h1>
         <p>{{ attr.date }}</p>
@@ -13,22 +13,61 @@
       </header>
       <component :is="dynamicComponent" />
     </article>
+    <div v-else>
+      <h1>ブログ</h1>
+      <div v-for="(attr, key) in blogMdAttrs" :key="key" class="">
+        <article>
+          <div class="">
+            <img :src="attr.image" :alt="attr.title">
+          </div>
+          <div class="">
+            <p class="title">
+              <nuxt-link :to="`/blog/${attr.name}`">
+                {{ attr.title }}
+              </nuxt-link>
+            </p>
+            <p> {{ attr.date }} </p>
+            <p class="description">
+              {{ attr.description }}
+            </p>
+          </div>
+        </article>
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { createComponent, SetupContext } from '@vue/composition-api'
 
+interface MarkdownAttrWithName extends MarkdownAttr {
+  name: string
+}
+
 export default createComponent({
   components: {
   },
   setup (_, context: SetupContext) {
-    const slug = context.root.$root.$route.params.slug
+    function importAll (resolve: any, attrs: MarkdownAttrWithName[]) {
+      resolve.keys().forEach((key: any) => {
+        const [, name] = key.match(/\/(.+)\.md$/)
+        attrs.push({ ...resolve(key).attributes, name })
+      })
+    }
+
+    const slug = context.root.$root.$route.params.slug || ''
+
+    if (slug === '') {
+      const blogMdAttrs: MarkdownAttrWithName[] = []
+      importAll(require.context('~/contents/blog', true, /\.md$/), blogMdAttrs)
+      return { blogMdAttrs, slug }
+    }
+
     // https://hmsk.github.io/frontmatter-markdown-loader/vue.html
     const md = require(`~/contents/blog/${slug}.md`)
     const attr: MarkdownAttr = md.attributes
     const dynamicComponent = md.vue.component
-    return { attr, dynamicComponent }
+    return { attr, dynamicComponent, slug }
   }
 })
 </script>

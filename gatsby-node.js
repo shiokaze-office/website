f975ebb92e2a876b5e56e12fd05f39a2fd30c710
linyows <linyows@gmail.com>
2020-05-14 22:54:30 +0900

    integrate to gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,5 +4,53 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
-exports.createPages = require('./gatsby/createPages')
-exports.onCreateNode = require('./gatsby/onCreateNode')
+const replacePath = path => (path === `/` ? path : path.replace(/\/$/, ``))
+const { createFilePath } = require(`gatsby-source-filesystem`)
+const path = require("path")
+
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  const { createNodeField } = actions
+  if (node.internal.type === `MarkdownRemark`) {
+    const slug = createFilePath({ node, getNode, basePath: `pages` })
+    createNodeField({
+      node,
+      name: `slug`,
+      value: replacePath(slug),
+    })
+  }
+}
+
+exports.createPages = async ({ actions, graphql, reporter }) => {
+  const { createPage } = actions
+  const page = path.resolve(`src/templates/page.tsx`)
+  const post = path.resolve(`src/templates/post.tsx`)
+  const result = await graphql(`
+    {
+      allMarkdownRemark(
+        sort: { order: DESC, fields: [frontmatter___date] }
+        limit: 1000
+      ) {
+        edges {
+          node {
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  if (result.errors) {
+    reporter.panicOnBuild(`Error while running GraphQL query.`)
+    return
+  }
+
+  result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+    createPage({
+      path: replacePath(node.fields.slug),
+      component: node.fields.slug.match("/blog/") ? post : page,
+      context: {}, // additional data can be passed via context
+    })
+  })
+}
--- gatsby/utils.js
+++
@@ -1,2 +0,0 @@
-// Replacing '/' would result in empty string which is invalid
-module.exports = replacePath = path => (path === `/` ? path : path.replace(/\/$/, ``))

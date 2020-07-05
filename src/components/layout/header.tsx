92c5709080aae7a73fb821eafbff9b067920a975
linyows <linyows@gmail.com>
2020-07-05 17:18:52 +0900

    support burger menu

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link, useStaticQuery, graphql } from 'gatsby'
 import styled from 'styled-components'
 import logo from './shiokaze.svg'
@@ -16,11 +16,15 @@ const Component: React.FC = () => {
     }
   `)
 
+  const [open, setOpen] = useState(false)
+
   return (
-    <Header>
+    <div>
+      <Header open={open}>
         <Logo>
           <Link to="/"> <img src={logo} alt={title} /> </Link>
         </Logo>
+
         <Nav>
           <Link to="/will">遺言</Link>
           <Link to="/inheritance">相続手続き</Link>
@@ -29,24 +33,63 @@ const Component: React.FC = () => {
           <Link to="/family-trust">家族信託</Link>
           <Link to="/blog">ブログ</Link>
         </Nav>
+
         <About>
           <Button href="/about">行政書士紹介</Button>
           <Button href="/office">事務所案内</Button>
         </About>
-    </Header>
+
+        <Burger>
+          <button onClick={() => setOpen(!open)} aria-expanded="false" aria-controls="navigation" aria-label="Navigation" aria-haspopup="true">
+            <svg viewBox="0 0 40 40" fill="none">
+              <circle cx="20" cy="20" r="19.5" stroke="currentColor"></circle>
+              {open === false &&
+                <path fill="currentColor" d="M12 16v-2h17v2zM12 21v-2h17v2zM12 26v-2h17v2z"></path>
+              }
+              {open === true &&
+                <path d="M14 14l13 12m-13 0l13-12" stroke="currentColor" stroke-width="2"></path>
+              }
+            </svg>
+          </button>
+        </Burger>
+
+        <Nav4Mob open={open}>
+          <div class="job">
+            <Link to="/will">遺言</Link>
+            <Link to="/inheritance">相続手続き</Link>
+            <Link to="/guardianship">任意後見契約</Link>
+            <Link to="/entrusted-business">事務委任契約</Link>
+            <Link to="/family-trust">家族信託</Link>
+            <Link to="/blog">ブログ</Link>
+          </div>
+          <div class="info">
+            <Button href="/about">行政書士紹介</Button>
+            <Button href="/office">事務所案内</Button>
+          </div>
+        </Nav4Mob>
+      </Header>
+      <Nav4MobLayer open={open} />
+    </div>
   )
 }
 
 const Header = styled.header`
   display: grid;
   grid-template-columns: 22rem 1fr 22rem;
-  margin: 0 var(--gutter);
+  margin: ${({ open }) => (open ? "0" : "0 var(--gutter)")};
+  padding: ${({ open }) => (open ? "0 var(--gutter)" : "0")};
   padding-top: 2.5rem;
   padding-bottom: 1.25rem;
   align-items: end;
   border-bottom: 1px solid #000;
+  position: relative;
+  z-index: 4;
+  background-color: #fff;
   @media screen and (max-width:480px) {
-    grid-template-columns: 1fr;
+    grid-template-columns: 1fr 2rem;
+    align-items: center;
+    padding-top: 1.5rem;
+    padding-bottom: 1rem;
   }
 `
 const Logo = styled.p`
@@ -54,13 +97,19 @@ const Logo = styled.p`
   margin: 0;
   img {
     margin: 0;
-    width: 18rem;
+    width: 15rem;
     &:hover {
       opacity: 0.8;
     }
   }
   a {
     display: block;
+    line-height: 1;
+  }
+  @media screen and (max-width:480px) {
+    img {
+      width: 17rem;
+    }
   }
 `
 const Nav = styled.nav`
@@ -73,6 +122,9 @@ const Nav = styled.nav`
       opacity: 0.8;
     }
   }
+  @media screen and (max-width:480px) {
+    display: none;
+  }
 `
 const About = styled.nav`
   text-align: right;
@@ -83,6 +135,69 @@ const About = styled.nav`
   p {
     display: inline;
   }
+  @media screen and (max-width:480px) {
+    display: none;
+  }
+`
+const Burger = styled.div`
+  display: none;
+  @media screen and (max-width:480px) {
+    display: block;
+    width: 2rem;
+    line-height: 1;
+    button {
+      margin: 0;
+      padding: 0;
+      width: 2rem;
+      border: none;
+      background: transparent;
+      cursor: pointer;
+    }
+    svg {
+      width: 2rem;
+      display: block;
+    }
+  }
+`
+const Nav4Mob = styled.div`
+  display: none;
+  position: absolute;
+  bottom: -1px;
+  left: 0;
+  width: 100%;
+  background-color: #fff;
+  z-index: 4;
+  max-height: calc(0rem + 100vh);
+  transition: max-height 350ms ease 0s;
+  overflow-y: auto;
+  .job {
+    padding-bottom: var(--gutter);
+    a {
+      display: block;
+      padding: var(--gutter);
+      border-bottom: 1px solid #000;
+    }
+  }
+  .info {
+    margin-bottom: var(--gutter);
+    padding-bottom: var(--gutter);
+    p {
+      display: inline;
+      margin-left: var(--gutter);
+    }
+  }
+  @media screen and (max-width:480px) {
+    display: ${({ open }) => (open ? "block" : "none")};
+    transform: ${({ open }) => (open ? "translateY(100%)" : "translateY(0)")};
+  }
+`
+const Nav4MobLayer = styled.div`
+  display: ${({ open }) => (open ? "block" : "none")};
+  position: fixed;
+  inset: 0px;
+  background: rgba(0, 0, 0, 0.5) none repeat scroll 0% 0%;
+  z-index: 3;
+  transition: background 350ms ease 0s;
 `
 
 export default Component
--- types/graphql-types.d.ts
+++ types/graphql-types.d.ts
@@ -1922,14 +1922,14 @@ export type QuerySitePageArgs = {
   internalComponentName?: Maybe<StringQueryOperatorInput>;
   componentChunkName?: Maybe<StringQueryOperatorInput>;
   matchPath?: Maybe<StringQueryOperatorInput>;
-  id?: Maybe<StringQueryOperatorInput>;
-  parent?: Maybe<NodeFilterInput>;
-  children?: Maybe<NodeFilterListInput>;
-  internal?: Maybe<InternalFilterInput>;
   isCreatedByStatefulCreatePages?: Maybe<BooleanQueryOperatorInput>;
   pluginCreator?: Maybe<SitePluginFilterInput>;
   pluginCreatorId?: Maybe<StringQueryOperatorInput>;
   componentPath?: Maybe<StringQueryOperatorInput>;
+  id?: Maybe<StringQueryOperatorInput>;
+  parent?: Maybe<NodeFilterInput>;
+  children?: Maybe<NodeFilterListInput>;
+  internal?: Maybe<InternalFilterInput>;
 };
 
 
@@ -1946,8 +1946,6 @@ export type QuerySiteArgs = {
   siteMetadata?: Maybe<SiteSiteMetadataFilterInput>;
   port?: Maybe<DateQueryOperatorInput>;
   host?: Maybe<StringQueryOperatorInput>;
-  polyfill?: Maybe<BooleanQueryOperatorInput>;
-  pathPrefix?: Maybe<StringQueryOperatorInput>;
   id?: Maybe<StringQueryOperatorInput>;
   parent?: Maybe<NodeFilterInput>;
   children?: Maybe<NodeFilterListInput>;
@@ -2059,8 +2057,6 @@ export type Site = Node & {
   siteMetadata?: Maybe<SiteSiteMetadata>;
   port?: Maybe<Scalars['Date']>;
   host?: Maybe<Scalars['String']>;
-  polyfill?: Maybe<Scalars['Boolean']>;
-  pathPrefix?: Maybe<Scalars['String']>;
   id: Scalars['ID'];
   parent?: Maybe<Node>;
   children: Array<Node>;
@@ -2273,8 +2269,6 @@ export type SiteFieldsEnum =
   | 'siteMetadata___author'
   | 'port'
   | 'host'
-  | 'polyfill'
-  | 'pathPrefix'
   | 'id'
   | 'parent___id'
   | 'parent___parent___id'
@@ -2367,8 +2361,6 @@ export type SiteFilterInput = {
   siteMetadata?: Maybe<SiteSiteMetadataFilterInput>;
   port?: Maybe<DateQueryOperatorInput>;
   host?: Maybe<StringQueryOperatorInput>;
-  polyfill?: Maybe<BooleanQueryOperatorInput>;
-  pathPrefix?: Maybe<StringQueryOperatorInput>;
   id?: Maybe<StringQueryOperatorInput>;
   parent?: Maybe<NodeFilterInput>;
   children?: Maybe<NodeFilterListInput>;
@@ -2390,14 +2382,14 @@ export type SitePage = Node & {
   internalComponentName: Scalars['String'];
   componentChunkName: Scalars['String'];
   matchPath?: Maybe<Scalars['String']>;
-  id: Scalars['ID'];
-  parent?: Maybe<Node>;
-  children: Array<Node>;
-  internal: Internal;
   isCreatedByStatefulCreatePages?: Maybe<Scalars['Boolean']>;
   pluginCreator?: Maybe<SitePlugin>;
   pluginCreatorId?: Maybe<Scalars['String']>;
   componentPath?: Maybe<Scalars['String']>;
+  id: Scalars['ID'];
+  parent?: Maybe<Node>;
+  children: Array<Node>;
+  internal: Internal;
 };
 
 export type SitePageConnection = {
@@ -2433,92 +2425,6 @@ export type SitePageFieldsEnum =
   | 'internalComponentName'
   | 'componentChunkName'
   | 'matchPath'
-  | 'id'
-  | 'parent___id'
-  | 'parent___parent___id'
-  | 'parent___parent___parent___id'
-  | 'parent___parent___parent___children'
-  | 'parent___parent___children'
-  | 'parent___parent___children___id'
-  | 'parent___parent___children___children'
-  | 'parent___parent___internal___content'
-  | 'parent___parent___internal___contentDigest'
-  | 'parent___parent___internal___description'
-  | 'parent___parent___internal___fieldOwners'
-  | 'parent___parent___internal___ignoreType'
-  | 'parent___parent___internal___mediaType'
-  | 'parent___parent___internal___owner'
-  | 'parent___parent___internal___type'
-  | 'parent___children'
-  | 'parent___children___id'
-  | 'parent___children___parent___id'
-  | 'parent___children___parent___children'
-  | 'parent___children___children'
-  | 'parent___children___children___id'
-  | 'parent___children___children___children'
-  | 'parent___children___internal___content'
-  | 'parent___children___internal___contentDigest'
-  | 'parent___children___internal___description'
-  | 'parent___children___internal___fieldOwners'
-  | 'parent___children___internal___ignoreType'
-  | 'parent___children___internal___mediaType'
-  | 'parent___children___internal___owner'
-  | 'parent___children___internal___type'
-  | 'parent___internal___content'
-  | 'parent___internal___contentDigest'
-  | 'parent___internal___description'
-  | 'parent___internal___fieldOwners'
-  | 'parent___internal___ignoreType'
-  | 'parent___internal___mediaType'
-  | 'parent___internal___owner'
-  | 'parent___internal___type'
-  | 'children'
-  | 'children___id'
-  | 'children___parent___id'
-  | 'children___parent___parent___id'
-  | 'children___parent___parent___children'
-  | 'children___parent___children'
-  | 'children___parent___children___id'
-  | 'children___parent___children___children'
-  | 'children___parent___internal___content'
-  | 'children___parent___internal___contentDigest'
-  | 'children___parent___internal___description'
-  | 'children___parent___internal___fieldOwners'
-  | 'children___parent___internal___ignoreType'
-  | 'children___parent___internal___mediaType'
-  | 'children___parent___internal___owner'
-  | 'children___parent___internal___type'
-  | 'children___children'
-  | 'children___children___id'
-  | 'children___children___parent___id'
-  | 'children___children___parent___children'
-  | 'children___children___children'
-  | 'children___children___children___id'
-  | 'children___children___children___children'
-  | 'children___children___internal___content'
-  | 'children___children___internal___contentDigest'
-  | 'children___children___internal___description'
-  | 'children___children___internal___fieldOwners'
-  | 'children___children___internal___ignoreType'
-  | 'children___children___internal___mediaType'
-  | 'children___children___internal___owner'
-  | 'children___children___internal___type'
-  | 'children___internal___content'
-  | 'children___internal___contentDigest'
-  | 'children___internal___description'
-  | 'children___internal___fieldOwners'
-  | 'children___internal___ignoreType'
-  | 'children___internal___mediaType'
-  | 'children___internal___owner'
-  | 'children___internal___type'
-  | 'internal___content'
-  | 'internal___contentDigest'
-  | 'internal___description'
-  | 'internal___fieldOwners'
-  | 'internal___ignoreType'
-  | 'internal___mediaType'
-  | 'internal___owner'
-  | 'internal___type'
   | 'isCreatedByStatefulCreatePages'
   | 'pluginCreator___id'
   | 'pluginCreator___parent___id'
@@ -2620,7 +2526,93 @@ export type SitePageFieldsEnum =
   | 'pluginCreator___packageJson___peerDependencies___version'
   | 'pluginCreator___packageJson___keywords'
   | 'pluginCreatorId'
-  | 'componentPath';
+  | 'componentPath'
+  | 'id'
+  | 'parent___id'
+  | 'parent___parent___id'
+  | 'parent___parent___parent___id'
+  | 'parent___parent___parent___children'
+  | 'parent___parent___children'
+  | 'parent___parent___children___id'
+  | 'parent___parent___children___children'
+  | 'parent___parent___internal___content'
+  | 'parent___parent___internal___contentDigest'
+  | 'parent___parent___internal___description'
+  | 'parent___parent___internal___fieldOwners'
+  | 'parent___parent___internal___ignoreType'
+  | 'parent___parent___internal___mediaType'
+  | 'parent___parent___internal___owner'
+  | 'parent___parent___internal___type'
+  | 'parent___children'
+  | 'parent___children___id'
+  | 'parent___children___parent___id'
+  | 'parent___children___parent___children'
+  | 'parent___children___children'
+  | 'parent___children___children___id'
+  | 'parent___children___children___children'
+  | 'parent___children___internal___content'
+  | 'parent___children___internal___contentDigest'
+  | 'parent___children___internal___description'
+  | 'parent___children___internal___fieldOwners'
+  | 'parent___children___internal___ignoreType'
+  | 'parent___children___internal___mediaType'
+  | 'parent___children___internal___owner'
+  | 'parent___children___internal___type'
+  | 'parent___internal___content'
+  | 'parent___internal___contentDigest'
+  | 'parent___internal___description'
+  | 'parent___internal___fieldOwners'
+  | 'parent___internal___ignoreType'
+  | 'parent___internal___mediaType'
+  | 'parent___internal___owner'
+  | 'parent___internal___type'
+  | 'children'
+  | 'children___id'
+  | 'children___parent___id'
+  | 'children___parent___parent___id'
+  | 'children___parent___parent___children'
+  | 'children___parent___children'
+  | 'children___parent___children___id'
+  | 'children___parent___children___children'
+  | 'children___parent___internal___content'
+  | 'children___parent___internal___contentDigest'
+  | 'children___parent___internal___description'
+  | 'children___parent___internal___fieldOwners'
+  | 'children___parent___internal___ignoreType'
+  | 'children___parent___internal___mediaType'
+  | 'children___parent___internal___owner'
+  | 'children___parent___internal___type'
+  | 'children___children'
+  | 'children___children___id'
+  | 'children___children___parent___id'
+  | 'children___children___parent___children'
+  | 'children___children___children'
+  | 'children___children___children___id'
+  | 'children___children___children___children'
+  | 'children___children___internal___content'
+  | 'children___children___internal___contentDigest'
+  | 'children___children___internal___description'
+  | 'children___children___internal___fieldOwners'
+  | 'children___children___internal___ignoreType'
+  | 'children___children___internal___mediaType'
+  | 'children___children___internal___owner'
+  | 'children___children___internal___type'
+  | 'children___internal___content'
+  | 'children___internal___contentDigest'
+  | 'children___internal___description'
+  | 'children___internal___fieldOwners'
+  | 'children___internal___ignoreType'
+  | 'children___internal___mediaType'
+  | 'children___internal___owner'
+  | 'children___internal___type'
+  | 'internal___content'
+  | 'internal___contentDigest'
+  | 'internal___description'
+  | 'internal___fieldOwners'
+  | 'internal___ignoreType'
+  | 'internal___mediaType'
+  | 'internal___owner'
+  | 'internal___type';
 
 export type SitePageFilterInput = {
   path?: Maybe<StringQueryOperatorInput>;
@@ -2628,14 +2620,14 @@ export type SitePageFilterInput = {
   internalComponentName?: Maybe<StringQueryOperatorInput>;
   componentChunkName?: Maybe<StringQueryOperatorInput>;
   matchPath?: Maybe<StringQueryOperatorInput>;
-  id?: Maybe<StringQueryOperatorInput>;
-  parent?: Maybe<NodeFilterInput>;
-  children?: Maybe<NodeFilterListInput>;
-  internal?: Maybe<InternalFilterInput>;
   isCreatedByStatefulCreatePages?: Maybe<BooleanQueryOperatorInput>;
   pluginCreator?: Maybe<SitePluginFilterInput>;
   pluginCreatorId?: Maybe<StringQueryOperatorInput>;
   componentPath?: Maybe<StringQueryOperatorInput>;
+  id?: Maybe<StringQueryOperatorInput>;
+  parent?: Maybe<NodeFilterInput>;
+  children?: Maybe<NodeFilterListInput>;
+  internal?: Maybe<InternalFilterInput>;
 };
 
 export type SitePageGroupConnection = {
